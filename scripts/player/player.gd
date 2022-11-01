36a0517fa3ebd234e1716b3626fee1c8a34fa355
Green Mario <codeblackin1@gmail.com>
2022-11-01 11:05:18 -0300

    initial commit

---
+++ scripts/player/player.gd
@@ -0,0 +1,170 @@
+extends KinematicBody2D
+class_name Player
+
+# Aqui estamos referenciando o nó Texture como valor da var player_sprite.
+onready var player_sprite: Sprite = get_node("Texture")
+onready var wall_ray: RayCast2D = get_node("WallRay")
+
+# Vector2 mexe com valores em dois eixos, o eixo x e o eixo y.
+var velocity: Vector2
+# Esta var começa com o valor de 1 porque a animação de wall slide tem a sprite olhando para a
+# esquerda.
+var direction: int = 1
+var jump_count: int = 0
+# Estas vars servem como flags (isso significa que elas vão retornar apenas um de dois valores).
+var land: bool = false
+var on_wall: bool = false
+var attacking: bool = false
+var defending: bool = false
+var crouching: bool = false
+# Esta var can_track_input serve para impedir que o jogador faça outras ações enquanto alguma das
+# vars acima forem true.
+var can_track_input: bool = true
+# Esta var serve para impedir que a animação de wall slide rode em momentos indevidos.
+var not_on_wall: bool = true
+
+
+# Estas export vars servem para que seja possível manipular o valor das vars sem ter que editar
+# diretamente no script (estes valores são manipulados na workspace).
+export(int) var speed
+export(int) var jump_speed
+export(int) var player_gravity
+export(int) var wall_jump_speed
+export(int) var wall_gravity
+# Esta var serve para que, quando o jogador pressionar o botão de pular no estado de wall slide, o
+# personagem pule na direção oposta a da parede para que assim ele não pule para cima e fique preso
+# na animação de wall slide.
+export(int) var wall_impulse_speed
+
+
+func _physics_process(delta: float):
+	horizontal_moviment_env()
+	vertical_moviment_env()
+	actions_env()
+	gravity(delta)
+	# Aqui a movimentação está sendo efetuada na cena com o move_and_slide().
+	# O move_and_slide() faz com que o personagem não rode a animação de run quando der de cara com
+	# uma parede.
+	# Vector2, em velocity, acessa a sua propriedade UP para que o move_and_slide() possa verificar
+	# interações não só com o chão/teto mas também interações com a parede.
+	# Com isso o problema do jump é resolvido, pois o mesmo pulava algumas vezes e depois não pulava
+	# mais porque o valor de jump_count era zerado e não atualizado por causa que o is_on_floor()
+	# não verificava parede como chão.
+	velocity = move_and_slide(velocity, Vector2.UP)
+	# Aqui está sendo chamada a var player_sprite para rodar as animações.
+	player_sprite.animate(velocity)
+
+
+# O sinal -> indica que a função está ou não retornando um valor. Se o que vier depois do sinal for
+# um valor do tipo void, então significa que a função não retorna nada.
+func horizontal_moviment_env() -> void:
+	# Esta função trata de toda a movimentação horizontal do personagem.
+	var input_direction: float = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	# Esta condição impede o jogador de se movimentar na horizontal caso ele esteja agachado,
+	# defendendo ou atacando.
+	if can_track_input == false or attacking:
+		velocity.x = 0
+		
+		return
+	
+	velocity.x = input_direction * speed
+
+
+func vertical_moviment_env() -> void:
+	# is_on_floor() é uma função da própria godot que verifica se o personagem está interagindo
+	# com algum objeto do tipo floor.
+	if is_on_floor() or is_on_wall():
+		jump_count = 0
+	# Esta var serve apenas para diminuir a linha da condição if abaixo.
+	var jump_condition: bool = can_track_input and not attacking
+	
+	# O is_action_just_pressed() verifica se um botão foi pressionado uma única vez.
+	if Input.is_action_just_pressed("ui_select") and jump_count < 2 and jump_condition:
+		# Aqui a condição verifica se o botão foi pressiona e se o valor da var jump_count é menor
+		# que 2.
+		jump_count += 1
+		# Esta condição faz com que o estado de wall slide seja executado.
+		if next_to_wall() and not is_on_floor():
+			velocity.y = wall_jump_speed
+			velocity.x += wall_impulse_speed * direction
+		else:
+			velocity.y = jump_speed
+	
+	
+func actions_env() -> void:
+	attack()
+	defense()
+	crouch()
+
+# Esta função faz o personagem atacar.
+func attack() -> void:
+	# Esta var verifica se o personagem está agachado, defendendo ou atacando. Se o personagem
+	# estiver fazendo alguma dessas ações, a var vai ficar com o valor de false e a animação de
+	# attack não será efetuada.
+	var attack_condition: bool = not attacking and not defending and not crouching
+	
+	if Input.is_action_just_pressed("attack") and attack_condition and is_on_floor():
+		attacking = true
+		player_sprite.normal_attack = true
+
+# Esta função faz o personagem defender.
+func defense() -> void:
+	if Input.is_action_pressed("defense") and is_on_floor() and not crouching:
+		defending = true
+		can_track_input = false
+	elif not crouching:
+		defending = false
+		can_track_input = true
+		player_sprite.shield_off = true
+
+# Esta função faz o personagem agachar.	
+func crouch() -> void:
+	if Input.is_action_pressed("crouch") and is_on_floor() and not defending:
+		crouching = true
+		can_track_input = false
+	elif not defending:
+		crouching = false
+		can_track_input = true
+		player_sprite.crouching_off = true
+
+func next_to_wall():
+	# Esta condição chama uma propriedade do nó RayCast2D para verificar a colisão e retornar um
+	# valor booleano.
+	if wall_ray.is_colliding() and not is_on_floor():
+		# Esta condição será chamada toda vez que o personagem estiver colidindo com uma parede. A
+		# velocidade em y é definida para 0 para evitar que, ao rodar a animação de wall slide, o
+		# personagem não deslize para cima por causa da força do pulo estar maior que a força da
+		# gravidade. Logo após, a var not_on_wall recebe false para que a condição seja ignorada e
+		# não ocorra bugs no código.
+		if not_on_wall:
+			velocity.y = 0
+			not_on_wall = false
+			
+		return true
+	else:
+		# Aqui a var not_on_wall recebe true quando o personagem não estiver mais colidindo com uma
+		# parede.
+		not_on_wall = true
+		
+		# O else retorna false para que a animação de wall slide não rode caso o personagem esteja
+		# colidindo com um chão.
+		return false
+
+
+func gravity(delta: float) -> void:
+	# Esta condição segue a mesma lógica do else logo abaixo, porém é aplicada apenas se a
+	# função chamada retornar true.
+	if next_to_wall():
+		velocity.y += wall_gravity * delta
+		
+		# Esta condição segue a mesma lógica da condição que se encontra dentro do else.
+		if velocity.y >= wall_gravity:
+			velocity.y = wall_gravity
+	else:
+		# Aqui está sendo setada a movimentação no eixo y do personagem.
+		velocity.y += delta * player_gravity
+		
+		# Essa condição é feita para evitar que o personagem acabe ficando muito rápido quando estiver
+		# caindo, se movendo positivamente no eixo y. 
+		if velocity.y >= player_gravity:
+			velocity.y = player_gravity
---
+++ scripts/player/player_texture.gd
@@ -0,0 +1,100 @@
+extends Sprite
+class_name PlayerTexture
+
+# Aqui está sendo pego o nó AnimationPlayer e o colocando como valor de animation.
+# Foi feita dessa forma porque AnimationPlayer é irmão de Sprite, e por causa disso a "conversa"
+# entre eles é mais difícil.
+# Assim a var animation tem acesso as propriedades do nó AnimationPlayer.
+export(NodePath) onready var animation = get_node(animation) as AnimationPlayer
+export(NodePath) onready var player = get_node(player) as KinematicBody2D
+# Esta var serve para indicar para qual direção o personagem está atacando.
+var suffix: String = "Right"
+# Esta var serve para dizer ao código que o personagem está efetuando um ataque normal.
+var normal_attack: bool = false
+# As vars shield_off e crouching_off servem para prender o personagem no último frame das animações
+# enquantos eles estiverem com o botão pressionado.
+var shield_off: bool = false
+var crouching_off: bool = false
+
+# Aqui se encontra a função que é chamada para rodar as animações e flipar as sprites.
+func animate(direction: Vector2) -> void:
+	verify_position(direction)
+	
+	if player.attacking or player.defending or player.crouching or player.next_to_wall():
+		action_behavior()
+	elif direction.y != 0:
+		vertical_behavior(direction)
+	elif player.land == true:
+		animation.play("Land")
+		# Aqui foi acessada o _physics_process() de player e o setamos como false para que quando
+		# a animação de land estiver rodando, o player não consiga se movimentar.
+		player.set_physics_process(false)
+	else:
+		horizontal_behavior(direction)
+	
+	# Esta função faz o personagem flipar de acordo com o valor de x em direction.
+func verify_position(direction: Vector2) -> void:
+	if direction.x > 0:
+		flip_h = false
+		suffix = "Right"
+		# Aqui a var direction de player recebe -1 por causa do pulo na parede. O valor precisa ser
+		# negativo para que o pulo ocorra na direção oposta a da parede.
+		player.direction = -1
+		# Aqui a position recebe um vetor zerado para não ocorrer um bug em relação a posição da
+		# sprite com o raycast e as animações.
+		position = Vector2.ZERO
+		# Aqui a propriedade cast_to de RayCast2D está recebendo este valor para apontar para a
+		# direção correta de acordo com a direção que o personagem estiver olhando.
+		player.wall_ray.cast_to = Vector2(5.5, 0)
+	elif direction.x < 0:
+		flip_h = true
+		suffix = "Left"
+		player.direction = 1
+		position = Vector2(-2, 0)
+		# Aqui o cast_to recebe -7.5 porque position recebe -2 (-5.5 + -2).
+		player.wall_ray.cast_to = Vector2(-7.5, 0)
+
+# Esta função roda as animações do personagem de acordo com o valor de x em direction.
+func horizontal_behavior(direction: Vector2) -> void:
+	if direction.x != 0:
+		animation.play("Run")
+	else:
+		animation.play("Idle")
+
+
+func action_behavior() -> void:
+	if player.next_to_wall():
+		animation.play("WallSlide")
+	elif player.attacking and normal_attack:
+		animation.play("Attack" + suffix)
+	elif player.defending and shield_off:
+		animation.play("Shield")
+		shield_off = false
+	elif player.crouching and crouching_off:
+		animation.play("Crouch")
+		crouching_off = false
+
+func vertical_behavior(direction: Vector2) -> void:
+	if direction.y > 0:
+		player.land = true
+		animation.play("Fall")
+	elif direction.y < 0:
+		animation.play("Jump")
+
+# Este signal foi chamado no código para que a animação de land possa ser interrompida e não ficar
+# em loop.
+func _on_animation_finished(anim_name: String):
+	match anim_name:
+		"Land":
+			player.land = false
+			# Aqui o _physics_process() volta a ser true para que as físicas do jogo voltem ao
+			# normal quando a animação de land acabar.
+			player.set_physics_process(true)
+			
+		"AttackLeft":
+			normal_attack = false
+			player.attacking = false
+		
+		"AttackRight":
+			normal_attack = false
+			player.attacking = false
